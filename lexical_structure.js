90bd407f8627eee7d046a2f998adf3aa777975ed
spandiyan25 <ksoundharapandiyan25@gmail.com>
2024-08-04 01:22:24 +0530

    first commit on data types

---
+++ lexical_structure.js
@@ -0,0 +1,57 @@
+
+/* JAVASCRIPT IS A CASE SENSITIVE LANGUAGE */
+
+
+/*              DATA TYPES 
+
+NUMBERS -> 1
+REAL NUMBER -> 0.1
+STRING -> "SOUNDHAR"
+DELIMITED STRING -> 'SOUNDHAR'
+BOOLEAN -> TRUE , FALSE
+NULL
+UNDIFINED
+
+ARRAY -> [0,1,2,3,4]
+OBJECT -> {key: value}
+
+*/
+
+/* Identifiers can be started with _ (underscore)  or $ (dollar) */
+
+console.log( new Date().toLocaleTimeString())
+
+/*  Math functions
+
+    Math.pow(2,3) => 8
+    Math.round(.6) => 1.0 nearest integer
+    Math.ceil(.6) => 1.0 nearest interger
+    Math.floor(.6) => 0.0 nearest lower integer
+    Math.abs(-5) => 5 negative number will be converted into positive
+    Math.max(23,45, 67,25) => 67
+    Math.min(23,45, 67,25) => 23
+    Math.PI
+    Math.random() => 0 to 1 -> 0.123
+    Math.sqrt(4) => 2
+    Math.pow(2,1/3) => 8 cubic root of 2.
+    Math.log(10)  => log value of 10.
+
+*/
+
+/*              DATE
+
+var now = new Date() -> current date and time
+var later = new Date(2010,0,1,17,10,30);
+
+    later.getFullYear();
+    later.getMonth();
+    later.getDate();
+    later.getDay();
+    later.getHours();
+    later.getUTCHours();
+    later.toString();
+    later.toLocaleDateString();
+    later.toLocaleTimeString();
+
+*/
+
